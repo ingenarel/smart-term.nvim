c84051d2f7583c03e955493a97fe9e641098089a
ingenarel <ingenarelitems@gmail.com>
2025-04-15 19:51:51 +0600

    (feat): add zellij

--- lua/smart-floatterm/init.lua
+++ lua/smart-floatterm/init.lua
@@ -38,9 +38,38 @@ function m.openTmuxTerm(command)
 	})
 end
 
+function m.openZellijTerm(command)
+	---@type integer
+	local floatingWinWidth = math.floor(vim.o.columns / 100 * m.widthPercentage)
+	---@type integer
+	local floatingWinHeight = math.floor(vim.o.lines / 100 * m.heightPercentage)
+	local execute = {
+		"zellij",
+		"action",
+		"new-pane",
+		"--floating",
+		"--width",
+		tostring(floatingWinWidth),
+		"--height",
+		tostring(floatingWinHeight),
+		"-x",
+		tostring(math.floor((vim.o.columns - floatingWinWidth - 2) / 2)),
+		"-y",
+		tostring(math.floor((vim.o.lines - floatingWinHeight + 3) / 2)),
+	}
+	if command ~= nil then
+		table.insert(execute, "--close-on-exit")
+		table.insert(execute, "--")
+		table.insert(execute, command)
+	end
+	vim.system(execute)
+end
+
 function m.open()
 	if os.getenv("TMUX") then
 		m.openTmuxTerm()
+	elseif os.getenv("ZELLIJ") then
+		m.openZellijTerm()
 	else
 		m.openNeovimTerm()
 	end
