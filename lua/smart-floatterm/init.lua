64da71ea3151701ef30bac29a3a3f52451a1dc38
ingenarel <ingenarelitems@gmail.com>
2025-04-20 00:52:56 +0600

    (refactor): call function in function arg
    bufID wasn't being used anywhere at the moment, so should be a little
    bit better

--- lua/smart-floatterm/init.lua
+++ lua/smart-floatterm/init.lua
@@ -6,8 +6,7 @@ function m.openNeovimTerm(command)
 	---@type integer
 	local floatingWinHeight = math.floor(vim.o.lines / 100 * m.heightPercentage)
 	---@type integer
-	local bufID = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_open_win(bufID, true, {
+	vim.api.nvim_open_win(vim.api.nvim_create_buf(false, true), true, {
 		relative = "editor",
 		width = floatingWinWidth,
 		height = floatingWinHeight,
