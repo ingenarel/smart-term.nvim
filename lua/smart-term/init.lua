5d33880421a8942aea39b4db880bf7f83fd67297
ingenarel <ingenarelitems@gmail.com>
2025-09-02 00:06:30 +0600

    (feat): have the ability to give choices for float panes

--- lua/smart-term/init.lua
+++ lua/smart-term/init.lua
@@ -16,35 +16,29 @@ function m.float(opts) -- {{{
 
     opts.command = opts[1] or opts.command
 
-    if os.getenv("TMUX") then
-        require("smart-term.tmux").float {
-            command = opts.command,
-            closeOnExit = opts.closeOnExit,
-            heightPercentage = opts.heightPercentage,
-            widthPercentage = opts.widthPercentage,
-            xOffset = opts.xOffset,
-            yOffset = opts.yOffset,
-            stopVim = opts.stopVim,
-        }
-    elseif os.getenv("ZELLIJ") then
-        require("smart-term.zellij").float {
-            command = opts.command,
-            closeOnExit = opts.closeOnExit,
-            heightPercentage = opts.heightPercentage,
-            widthPercentage = opts.widthPercentage,
-            xOffset = opts.xOffset,
-            yOffset = opts.yOffset,
-            stopVim = opts.stopVim,
-        }
-    else
-        require("smart-term.neovim").float {
-            command = opts.command,
-            heightPercentage = opts.heightPercentage,
-            widthPercentage = opts.widthPercentage,
-            xOffset = opts.xOffset,
-            yOffset = opts.yOffset,
-            closeOnExit = opts.closeOnExit,
-        }
+    local functions = {
+        tmux = function()
+            if os.getenv("TMUX") then
+                return require("smart-term.tmux").float
+            end
+        end,
+        zellij = function()
+            if os.getenv("ZELLIJ") then
+                return require("smart-term.zellij").float
+            end
+        end,
+        nvim = function()
+            return require("smart-term.neovim").float
+        end,
+    }
+
+    opts.choices = opts.choices or { "tmux", "zellij", "nvim" }
+    for _, choice in ipairs(opts.choices) do
+        local func = functions[choice]()
+        if func then
+            func(opts)
+            break
+        end
     end
 end -- }}}
 
